04f7424fc2bf7a56dc6725fa034a293aa4d3ba7b
atcoder8 <atcoder8@gmail.com>
2024-07-07 08:10:14 +0900

    Added error messages.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{fs, io::Write};
 
+use anyhow::Context;
 use clap::Parser;
 
 mod command;
@@ -9,22 +10,29 @@ fn main() -> anyhow::Result<()> {
     let cli = command::Cli::parse();
 
     print!("Reading original image ... ");
-    let original_image = image::open(&cli.original_image)?.into_rgba8();
+    let original_image = image::open(&cli.original_image)
+        .with_context(|| "Failed to load the original image.")?
+        .into_rgba8();
     println!("OK");
 
     // Creates output directory for processed images.
-    fs::create_dir_all(&cli.output_dir)?;
+    fs::create_dir_all(&cli.output_dir)
+        .with_context(|| "Failed to create output directory for processed images.")?;
 
     let opaque_image = processing::create_opaque_image(&original_image);
     print!("Saving opaque image ... ");
     std::io::stdout().flush().unwrap();
-    opaque_image.save(cli.output_dir.join("opaque.png"))?;
+    opaque_image
+        .save(cli.output_dir.join("opaque.png"))
+        .with_context(|| "Failed to save the opaque image.")?;
     println!("OK");
 
     let header_image = processing::create_header_image(&original_image);
     print!("Saving header image ... ");
     std::io::stdout().flush().unwrap();
-    header_image.save(cli.output_dir.join("header.png"))?;
+    header_image
+        .save(cli.output_dir.join("header.png"))
+        .with_context(|| "Failed to save processed image for header.")?;
     println!("OK");
 
     Ok(())
